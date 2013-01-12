c71a526aa9842982672c1d1379d029d903313410
James Martinez <jamescmartinez@gmail.com>
2013-01-11 23:50:05 -0800

    Updated Copyright

--- JMSimpleDate/NSDate+JMSimpleDate.h
+++ JMSimpleDate/NSDate+JMSimpleDate.h
@@ -1,6 +1,6 @@
 // NSDate+JMSimpleDate.h
 //
-// Copyright (c) 2012 James Martinez
+// Copyright (c) 2013 James Martinez
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
